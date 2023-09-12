be5930f61790e64a49ace2c9acd75060f1e535da
moohiit <pmohit645@gmail.com>
2023-09-12 14:29:55 +0530

    Full course

---
+++ Module 2- Python Basic Constructs (Assignments)/module1.py
@@ -0,0 +1,14 @@
+def addNumber(a, b):
+	print("Sum is ", a + b)
+
+def subtractNumber(a, b):
+	print("Difference is ", a-b)
+
+def multiplyNumber(a, b):
+	print("Product is ", a * b)
+
+def divideNumber(a, b):
+	print("Division is ", a / b)
+
+def modulusNumber(a, b):
+	print("Remainder is ", a % b)
